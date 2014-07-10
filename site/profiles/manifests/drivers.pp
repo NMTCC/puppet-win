7f0dae4eaa96b7cc7fbe88a6a4c75c79e1245a5a
Bryan Hughes <khan@nmt.edu>
2014-07-10 14:10:42 -0600

    Add new drivers to puppet-win repo
    Add new collection of drivers to smb repo on puppetwin.
    Update local driver listing

--- site/profiles/manifests/drivers.pp
+++ site/profiles/manifests/drivers.pp
@@ -8,6 +8,11 @@ class profiles::drivers {
                 '\\puppet-win.nmt.edu\winshare\drivers\HP-G3110\hpxpg311.inf',
                 '\\puppet-win.nmt.edu\winshare\drivers\Arduino\arduino.inf',
                 '\\puppet-win.nmt.edu\winshare\drivers\Intel-82579V\e1c62x64.inf',
+                '\\puppet-win.nmt.edu\winshare\drivers\Intel-965\igdlh64.inf',
+                '\\puppet-win.nmt.edu\winshare\drivers\Intel-Pro1000\e1d62x64.inf',
+                '\\puppet-win.nmt.edu\winshare\drivers\nv_dispwu.inf_amd64_neutral_104ae97042f2a706\nv_dispwu.inf',
+                '\\puppet-win.nmt.edu\winshare\drivers\nvhda.inf_amd64_neutral_6fca61a1d3e2d202\nvhda.inf',
+                '\\puppet-win.nmt.edu\winshare\drivers\nvstusb.inf_amd64_neutral_bf149df106f49b54\vstusb.inf',
   ]
 
   $certs = [ '\\puppet-win.nmt.edu\winshare\drivers\Arduino\arduino.cat'
