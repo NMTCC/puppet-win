1d645ccb3b5ccb8ca129da88d17258e4a55151ab
Bryan Hughes <khan@nmt.edu>
2014-07-15 09:44:26 -0600

    Turn software on for template role

--- site/roles/manifests/workstation/template.pp
+++ site/roles/manifests/workstation/template.pp
@@ -3,7 +3,7 @@
 class roles::workstation::template {
   include profiles::itc
   include profiles::registry
-  # include profiles::software
+  include profiles::software
   #nclude profiles::users
   include profiles::grouppolicy::machine
   include profiles::sysprep
