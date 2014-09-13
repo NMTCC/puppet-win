76e67323195704e77136ee2a4122ede63f413665
dgraham <dgraham@nmt.edu>
2014-09-12 18:42:46 -0600

    install this MS math extension for Word

--- site/profiles/manifests/software.pp
+++ site/profiles/manifests/software.pp
@@ -10,5 +10,6 @@ class profiles::software {
   include profiles::software::nanomodeling
   include profiles::software::petromod
   include profiles::software::blueview
+  include profiles::software::mathaddin
 
 }
