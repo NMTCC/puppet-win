2a40af41e441235bf1a7617a03a93ce42b107ea5
dgraham <dgraham@nmt.edu>
2014-10-02 14:51:51 -0600

    installer is donked, removing it

--- site/profiles/manifests/software/blueview.pp
+++ site/profiles/manifests/software/blueview.pp
@@ -3,16 +3,6 @@
 #  Lists packages for chocolatey installation
 class profiles::software::blueview {
 
-  # BlueView
-  # Benjamin F McNeill <bmcneill@nmt.edu>
-  # Requested 11 Sept 2014
-  # InstallShield
-    package { 'BlueView':
-    ensure          => installed,
-    source          => '\\puppet-win.nmt.edu\winshare\packages\BlueView\BlueView.msi',
-    install_options => ['/qn'],
-  }
-
   file { 'C:\Users\Public\Desktop\BlueView.lnk':
     ensure          => 'absent',
     force           => 'true',
