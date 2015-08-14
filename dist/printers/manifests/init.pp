81a4a5558303e5b26af7d6ff5647c2ec56c80003
dgraham <dgraham@nmt.edu>
2015-08-13 21:02:38 -0600

    printer updates

--- dist/printers/manifests/init.pp
+++ dist/printers/manifests/init.pp
@@ -10,6 +10,7 @@ class printers {
   'library208',
   'library208-color',
   'msec187',
+  'presidents113',
   'south218',
   'speare4a',
   'speare4b',
@@ -19,14 +20,15 @@ class printers {
   'speare23a',
   'speare23b',
   'speare23-color',
+  'speare112',
   'speare116',
   'speare117',
   'speare142',
-  'speare146',
+  'torres130',
   'transparency',
-  'torres125',
   'weir128',
   'weir209',
+  'west219',
   ]
 
   printers::printer { $normalprinters : }#  ->
