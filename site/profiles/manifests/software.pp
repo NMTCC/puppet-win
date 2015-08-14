a6e28015c31385a341affbd770500ff04e088f82
dgraham <dgraham@nmt.edu>
2015-08-13 21:18:27 -0600

    shut off old sw manifests

--- site/profiles/manifests/software.pp
+++ site/profiles/manifests/software.pp
@@ -6,10 +6,10 @@
 class profiles::software {
 
   include profiles::software::chocolatey
-  include profiles::software::biochemlab
+#  include profiles::software::biochemlab
   include profiles::software::nanomodeling
   include profiles::software::petromod
-  include profiles::software::blueview
+#  include profiles::software::blueview
   include profiles::software::mathaddin
   include profiles::software::lanschool
   include profiles::software::delprof2
