ccec2849b4d026e62e2faccb3406d068552bd87c
dgraham <dgraham@nmt.edu>
2015-08-26 16:43:00 -0600

    oops

--- site/profiles/manifests/software/sas.pp
+++ site/profiles/manifests/software/sas.pp
@@ -4,7 +4,7 @@ class profiles::software::sas {
 
   case $hour {
     '00', '01', '02': {
-      exec { 'SAS reinstall',
+      exec { 'SAS reinstall':
         command => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe -quiet -responsefile \\replicon.nmt.edu\samba\sas\ResponseRecord.log',
         creates => 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SAS\SAS 9.4 (English).lnk',
       }
