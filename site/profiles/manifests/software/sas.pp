20e65c4e16dd5c945ea8f0f96aefc94ba1187f94
dgraham <dgraham@nmt.edu>
2015-08-26 15:59:59 -0600

    install sas anyway

--- site/profiles/manifests/software/sas.pp
+++ site/profiles/manifests/software/sas.pp
@@ -1,20 +1,13 @@
 class profiles::software::sas {
 
-  $hour = generate("/usr/local/bin/winhour")
-
-  case $hour {
-    '00', '01', '02': {
-      package { 'SAS 9.4':
-        ensure          => installed,
-        source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
-        install_options => [
-          '-quiet',
-          '-responsefile',
-          '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
-        ],
-      }
-    }
-    default: { $dummy = [ 'Wrong time for SAS.', ] }
+  package { 'SAS 9.4':
+    ensure          => installed,
+    source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
+    install_options => [
+      '-quiet',
+      '-responsefile',
+      '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
+    ],
   }
 
 }
