ea7fbfc8d0610583c0a4884d28019d9e87cebc51
dgraham <dgraham@nmt.edu>
2015-08-26 16:41:12 -0600

    turns out this is a reinstallation, so we have to exec

--- site/profiles/manifests/software/sas.pp
+++ site/profiles/manifests/software/sas.pp
@@ -4,14 +4,9 @@ class profiles::software::sas {
 
   case $hour {
     '00', '01', '02': {
-      package { 'SAS 9.4':
-        ensure          => installed,
-        source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
-        install_options => [
-          '-quiet',
-          '-responsefile',
-          '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
-        ],
+      exec { 'SAS reinstall',
+        command => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe -quiet -responsefile \\replicon.nmt.edu\samba\sas\ResponseRecord.log',
+        creates => 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SAS\SAS 9.4 (English).lnk',
       }
     }
     default: { $dummy = [ 'Wrong time for SAS.', ] }
