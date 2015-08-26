7b9326447a61eb853819920e3cc88c6b7357c075
dgraham <dgraham@nmt.edu>
2015-08-26 15:46:44 -0600

    run sas everywhere

--- site/profiles/manifests/software/sas.pp
+++ site/profiles/manifests/software/sas.pp
@@ -2,24 +2,19 @@ class profiles::software::sas {
 
   $hour = generate("/usr/local/bin/winhour")
 
-  case $::hostname {
-    /^WEIR209/: {
-      case $hour {
-        '00', '01', '02': {
-          package { 'SAS 9.4':
-            ensure          => installed,
-            source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
-            install_options => [
-              '-quiet',
-              '-responsefile',
-              '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
-            ],
-          }
-        }
-        default: { $time = [ 'Wrong time for SAS.', ] }
+  case $hour {
+    '00', '01', '02': {
+      package { 'SAS 9.4':
+        ensure          => installed,
+        source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
+        install_options => [
+          '-quiet',
+          '-responsefile',
+          '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
+        ],
       }
     }
-    default: { $dummy = [ 'Did not install SAS.', ] }
+    default: { $dummy = [ 'Wrong time for SAS.', ] }
   }
 
 }
