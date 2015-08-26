f6aef5e5aea374dad5b38ba8e7a9b13625912b98
dgraham <dgraham@nmt.edu>
2015-08-26 16:25:02 -0600

    Revert "install sas anyway"
    This reverts commit 20e65c4e16dd5c945ea8f0f96aefc94ba1187f94.

--- site/profiles/manifests/software/sas.pp
+++ site/profiles/manifests/software/sas.pp
@@ -1,13 +1,20 @@
 class profiles::software::sas {
 
-  package { 'SAS 9.4':
-    ensure          => installed,
-    source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
-    install_options => [
-      '-quiet',
-      '-responsefile',
-      '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
-    ],
+  $hour = generate("/usr/local/bin/winhour")
+
+  case $hour {
+    '00', '01', '02': {
+      package { 'SAS 9.4':
+        ensure          => installed,
+        source          => '\\replicon.nmt.edu\samba\sas\SAS_9.4\setup.exe',
+        install_options => [
+          '-quiet',
+          '-responsefile',
+          '\\replicon.nmt.edu\samba\sas\ResponseRecord.log',
+        ],
+      }
+    }
+    default: { $dummy = [ 'Wrong time for SAS.', ] }
   }
 
 }
