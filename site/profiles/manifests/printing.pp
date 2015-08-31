9dbd7bf7221db74bb94fe3339e2eb41c2fe7e49c
dgraham <dgraham@nmt.edu>
2015-08-31 17:15:51 -0600

    fix printing w/net use as per kjm

---
+++ site/profiles/manifests/printing.pp
@@ -0,0 +1,29 @@
+# Class profiles::printing
+
+class profiles::printing {
+
+  $domainadmin = hiera('domainadmin')
+
+  file { 'c:\itc\etc\point_and_print.reg':
+    ensure  => 'file',
+    source  => 'puppet:///modules/profiles/printing/point_and_print.reg',
+  }
+
+  file { 'c:\itc\bin\AddPrintServerPrinters.ps1':
+    ensure  => 'file',
+    content => template('profiles/printing/AddPrintServerPrinters.ps1'),
+    require => File['c:\itc\etc\point_and_print.reg'],
+  }
+
+  exec { 'fixprinters':
+    command  => 'c:\itc\bin\AddPrintServerPrinters.ps1',
+    provider => powershell,
+    require  => File['c:\itc\bin\AddPrintServerPrinters.ps1'],
+  }
+
+  exec { 'printcleanup':
+    command  => '$f="c:\itc\bin\AddPrintServerPrinters.ps1"; If (Test-Path $f){ del $f }',
+    provider => powershell,
+  }
+
+}
---
+++ site/profiles/templates/printing/AddPrintServerPrinters.ps1
@@ -0,0 +1,48 @@
+﻿$log = "c:\itc\log\add_printers.log"
+$ErrorActionPreference
+$ErrorActionPreference = "Continue"
+get-date | %{$start = $_; $_; $_ > $log}
+
+#1. Disable the "Do you trust this printer?" driver installation dialogue
+#2. Remove Shut Down, Restart, Sleep, and Hibernate buttons
+#3. Disable display of last user name
+$reg_file = 'c:\itc\etc\point_and_print.reg'
+reg.exe import $reg_file$ | %{$_; $_ >> $log}
+
+$printserver = 'paperhost'
+
+#Get printers already added to this computer
+$added_printers = Get-WmiObject -Class Win32_Printer | where name -like "\\$printserver*" | select -expa ShareName | sort
+"Printers already added: $added_printers" | %{$_; $_ >> $log}
+
+#Printers to add from print server (with drivers on print server)
+$printers = 'cramer114 fidel130 fidel130-color library122 library208 library208-color library22 msec187 south218 speare116 speare117 speare142 speare16 speare23-color speare23a speare23b speare4a speare4b speare5 speare5-color transparency weir128 weir209'.split()
+"Target printers: $printers" | %{$_; $_ >> $log}
+
+$wnet = New-Object -ComObject WScript.Network
+
+#Login to paperhost as a TCC7 user
+$pw = '<%= @domainadmin %>'
+net.exe use \\paperhost $pw /user:tcc7\admin | %{$_; $_ >> $log}
+
+#For each targeted printer
+$printers | ForEach-Object {
+
+    #If printer is already added
+    if ($added_printers -ccontains $_) {
+        "$_ already added" | %{$_; $_ >> $log}
+    }
+    #Else add this printer
+    else {
+        $PortName = "\\$printserver\$_"
+        "Adding $PortName ..." | %{$_; $_ >> $log}
+        $wnet.AddWindowsPrinterConnection($PortName) | %{$_; $_ >> $log}
+    }
+}
+
+#Printers that need drivers added to print server
+$printers_without_drivers = 'presidents113 speare112 torres130 west219'.split()
+"Printers not targetted: $printers_without_drivers" | %{$_; $_ >> $log}
+
+get-date | %{$end = $_; $_; $_ >> $log}
+"Time for script to run was: $($end - $start)" | %{$_; $_ >> $log}
