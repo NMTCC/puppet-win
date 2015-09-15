111246f8398ab688c7f957f836ea2348f4471b73
dgraham <dgraham@nmt.edu>
2015-09-15 14:52:26 -0600

    properly exec disabling of firewall

--- site/profiles/manifests/firewall.pp
+++ site/profiles/manifests/firewall.pp
@@ -6,8 +6,22 @@
 class profiles::firewall {
 
   #Keep Firewall Disabled
-  exec { 'nofirewall':
-    command  => "netsh advfirewall set allprofiles state off",
+
+  exec { 'nodomainfw':
+    command  => '$(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(1) = $False',
+    unless   => 'if ( $(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(1) ) { exit 1 }',
+    provider => powershell,
+  }
+
+  exec { 'noprivatefw':
+    command  => '$(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(2) = $False',
+    unless   => 'if ( $(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(2) ) { exit 1 }',
+    provider => powershell,
+  }
+
+  exec { 'nopublicfw':
+    command  => '$(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(4) = $False',
+    unless   => 'if ( $(New-Object -ComObject HNetCfg.FwPolicy2).FirewallEnabled(4) ) { exit 1 }',
     provider => powershell,
   }
 
