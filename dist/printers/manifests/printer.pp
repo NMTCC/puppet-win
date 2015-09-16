df5ae8a7dcc61232cff3de0d5dd6c00464dfda38
dgraham <dgraham@nmt.edu>
2015-09-15 19:04:11 -0600

    try having puppet do the wscript printers

--- dist/printers/manifests/printer.pp
+++ dist/printers/manifests/printer.pp
@@ -1,7 +1,8 @@
 # Defines an easy type for installing Windows network printers.
-define printers::printer($queue=$title, $server='paperhost.nmt.edu') {
-  exec { "del-printer-${title}":
-    provider  => 'powershell',
-    command   => "rundll32 printui.dll,PrintUIEntry /dn /q /n\\\\${server}\\${queue}",
+define printers::printer($queue=$title, $server='paperhost') {
+  exec { "add-printer-${title}":
+    provider => 'powershell',
+    command  => "\$(New-Object -ComObject WScript.Network).AddWindowsPrinterConnection(\"\\\\${server}\\${title}\")",
+    onlyif   => "if ( \$(Get_WmiObject -Class Win32_Printer | Where { \$_.ShareName -match \"${title}\$\" } | Select -ExpandProperty ShareName) -eq \"${title}\" ) { exit 1 }",
   }
 }
