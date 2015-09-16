3233cfd4a07f8520985254b1371338343e3efcee
dgraham <dgraham@nmt.edu>
2015-09-15 19:15:15 -0600

    oops

--- dist/printers/manifests/printer.pp
+++ dist/printers/manifests/printer.pp
@@ -3,6 +3,6 @@ define printers::printer($queue=$title, $server='paperhost') {
   exec { "add-printer-${title}":
     provider => 'powershell',
     command  => "\$(New-Object -ComObject WScript.Network).AddWindowsPrinterConnection(\"\\\\${server}\\${title}\")",
-    onlyif   => "if ( \$(Get_WmiObject -Class Win32_Printer | Where { \$_.ShareName -match \"${title}\$\" } | Select -ExpandProperty ShareName) -eq \"${title}\" ) { exit 1 }",
+    onlyif   => "if ( \$(Get-WmiObject -Class Win32_Printer | Where { \$_.ShareName -Match \"${title}\$\" } | Select -ExpandProperty ShareName) -eq \"${title}\" ) { exit 1 }",
   }
 }
