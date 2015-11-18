23b10c1e593a2a691119669c6dae0f211a36ec55
dgraham <dgraham@nmt.edu>
2015-11-18 16:18:43 -0700

    try setting an owner on COMSOL license.dat

--- site/profiles/manifests/software/comsol.pp
+++ site/profiles/manifests/software/comsol.pp
@@ -4,6 +4,7 @@ class profiles::software::comsol {
   file { 'C:\Program Files\COMSOL\COMSOL51\Multiphysics\license\license.dat':
     ensure => 'file',
     source => '\\puppet-win.nmt.edu\winshare\packages\COMSOL\license.dat',
+    ownder => 'Administrators',
   }
 
 }
