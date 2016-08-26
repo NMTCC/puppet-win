82a48eb0994b62e3985cd45e857f4ef8965febff
dgraham <dgraham@nmt.edu>
2016-08-26 17:00:44 -0600

    oops

--- site/profiles/manifests/queues.pp
+++ site/profiles/manifests/queues.pp
@@ -32,7 +32,7 @@ class profiles::queues {
       mode   => '0644',
     }
     exec { "regadd-${title}":
-      command  => "reg add c:\\itc\\etc\\${title}.reg",
+      command  => "reg import c:\\itc\\etc\\${title}.reg",
       unless   => "reg query HKLM\\SYSTEM\\CurrentControlSet\\Control\\Print\\Printers\\${title}",
       provider => powershell,
       require  => File["C:/itc/etc/${title}.reg"],
