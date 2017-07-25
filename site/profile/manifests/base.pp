dbc378dc02baf491fa75d8f64076f522423ec874
dgraham <dgraham@nmt.edu>
2017-07-25 17:46:56 -0600

    remove unattend file

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,10 +1,19 @@
 # Configure basic stuff
 class profile::base {
 
-  $itcdirs = [ 'C:\itc', 'C:\itc\bin', 'C:\itc\etc', 'C:\itc\log', ]
-
-  file { $itcdirs:
+  file { 'C:\itc',
     ensure => directory,
+  }
+
+  file { [ 'C:\itc\bin', 'C:\itc\etc', 'C:\itc\log', ]:
+    ensure  => directory,
+    require => File['C:\itc'],
+  }
+
+  file { 'C:\itc\etc\unattend_sysprep.xml':
+    ensure  => absent,
+    backup  => false,
+    require => File['C:\itc\etc'],
   }
 
 }
