a677028f6aa6142cb25b3ed23d69256f436fc3a4
dgraham <dgraham@nmt.edu>
2017-08-18 11:45:39 -0600

    don't fail before fact exists

--- site/profile/manifests/software/local.pp
+++ site/profile/manifests/software/local.pp
@@ -3,34 +3,38 @@ class profile::software::local {
 
   include wget
 
-  $csrmd5 = $::facts['wgethashes']['ChangeScreenResolution.exe']
-  $dp2md5 = $::facts['wgethashes']['DelProf2.exe']
-  $pbmd5 = $::facts['wgethashes']['puppet.bat']
+  if $::wgethashes {
 
-  if $csrmd5 != '516d6e1c48f10a7104cc2a4604530c83' {
-    wget::fetch { 'ChangeScreenResolution.exe':
-      source      =>
-        'http://duplicon.nmt.edu/wingdows/ChangeScreenResolution_1.0.4/ChangeScreenResolution.exe',
-      destination => 'C:/itc/bin/',
-      require     => Package['wget'],
-    }
-  }
+    $csrmd5 = $::facts['wgethashes']['ChangeScreenResolution.exe']
+    $dp2md5 = $::facts['wgethashes']['DelProf2.exe']
+    $pbmd5 = $::facts['wgethashes']['puppet.bat']
 
-  if $dp2md5 != 'fdc4366be4d0e0d02c35b394ca59dd14' {
-    wget::fetch { 'DelProf2.exe':
-      source      =>
-        'http://duplicon.nmt.edu/wingdows/Delprof2_1.6.0/Delprof2%201.6.0/DelProf2.exe',
-      destination => 'C:/itc/bin/',
-      require     => Package['wget'],
+    if $csrmd5 != '516d6e1c48f10a7104cc2a4604530c83' {
+      wget::fetch { 'ChangeScreenResolution.exe':
+        source      =>
+          'http://duplicon.nmt.edu/wingdows/ChangeScreenResolution_1.0.4/ChangeScreenResolution.exe',
+        destination => 'C:/itc/bin/',
+        require     => Package['wget'],
+      }
     }
-  }
 
-  if $pbmd5 != 'f783e3cc12424cc4f427af85af30240e' {
-    wget::fetch { 'puppet.bat':
-      source      => 'http://duplicon.nmt.edu/wingdows/puppet.bat',
-      destination => 'C:/itc/bin/',
-      require     => Package['wget'],
+    if $dp2md5 != 'fdc4366be4d0e0d02c35b394ca59dd14' {
+      wget::fetch { 'DelProf2.exe':
+        source      =>
+          'http://duplicon.nmt.edu/wingdows/Delprof2_1.6.0/Delprof2%201.6.0/DelProf2.exe',
+        destination => 'C:/itc/bin/',
+        require     => Package['wget'],
+      }
     }
+
+    if $pbmd5 != 'f783e3cc12424cc4f427af85af30240e' {
+      wget::fetch { 'puppet.bat':
+        source      => 'http://duplicon.nmt.edu/wingdows/puppet.bat',
+        destination => 'C:/itc/bin/',
+        require     => Package['wget'],
+      }
+    }
+
   }
 
 }
