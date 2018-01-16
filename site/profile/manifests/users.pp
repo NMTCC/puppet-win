edaf55ac253a761dec4d443b13494ac7ef9877d4
dgraham <dgraham@nmt.edu>
2018-01-16 11:22:49 -0700

    create act roundabout

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -12,11 +12,25 @@ class profile::users {
     managehome => true,
     password   => hiera('winpassword'),
   }
+
+  exec { 'netuseract':
+    provider => powershell,
+    command  => 'net user /add act foo',
+    unless   => 'if (Get-LocalUser act) { exit 0 } else { exit 1 }',
+    notify   => Exec['localgroupact'],
+  }
+
+  exec { 'localgroupact':
+    provider    => powershell,
+    command     => 'net localgroup administrators act /add',
+    refreshonly => true,
+  }
   
   user { 'act':
     ensure     => present,
     managehome => true,
     password   => hiera('depassword'),
+    require    => Exec['netuseract'],
   }
   
 }
